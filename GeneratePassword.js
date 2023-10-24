05e76ef283a5763572d32e0f022274cd3c242bd1
Mosupatsela Moabelo <85935686+cosmicvariant@users.noreply.github.com>
2023-10-24 14:17:18 +0200

    Add files via upload
    Password Generator

---
+++ GeneratePassword.js
@@ -0,0 +1,17 @@
+function generatePassword(){
+    let password = "";
+    let length = 16;
+
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    
+    const arr = new Uint32Array(length);
+
+    window.crypto.getRandomValues(arr);
+    
+    for (let i = 0; i < length; i++) {
+        password += chars[arr[i] % chars.length];
+
+    }
+
+    return password;
+}
